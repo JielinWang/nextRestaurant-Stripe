aaf156e72bb29f0c1fec7543f888ff9450b53a52
Jielin Wang <94776104+JielinWang@users.noreply.github.com>
2022-11-21 15:20:47 -0600

    Add files via upload

---
+++ utils/urls.js
@@ -0,0 +1,6 @@
+export const API_URL =
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337";
+
+export const STRIPE_PK =
+  process.env.NEXT_PUBLIC_STRIPE_PK ||
+  "pk_test_51IHAcEG3rQIDZkUW4V3QMcKWuo0oUxHuYd1dVx5Q9Zhwjwzub6084fJImEVKmWjDFMrx14jvV9fbTuvHBQ6CKRoy00W07ZIGFJ"
